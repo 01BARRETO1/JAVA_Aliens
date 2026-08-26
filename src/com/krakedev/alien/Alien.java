8cc24ff58ec2d401c2968ae1e7831a78bca1ee80
01BARRETO1 <karbar_1990@hotmail.com>
2026-08-26 02:54:44 -0500

    clases Alien y TestConstructores

---
+++ src/com/krakedev/alien/Alien.java
@@ -0,0 +1,107 @@
+package com.krakedev.alien;
+
+public class Alien {
+
+	private int tamanio;
+	private String color;
+	private int numeroOjos;
+	private int numeroBrazos;
+	private int numeroPies;
+	private double precioExtremidad;
+	private double precioOjo;
+	private double precioCuerpo;
+
+	// constructor
+	public Alien(int tamanio, String color) {
+
+		this.color = color;
+		if (tamanio >= 5 && tamanio <= 30) {
+			this.tamanio = tamanio;
+		} else if (tamanio > 30) {
+			this.tamanio = 30;
+
+		} else if (tamanio < 5) {
+			this.tamanio = 5;
+		}
+		this.precioCuerpo = this.tamanio * 0.20;// Precio del cuerpo: 20% del tamaño
+		this.precioExtremidad = this.tamanio * 0.10;// Precio de extremidades: 10% del tamaño
+		this.precioOjo = this.tamanio * 0.05;// Precio de ojos: 5% del tamaño
+	}
+
+	// Crear métodos get para todos los atributos. No crear métodos set.
+
+	public int getTamanio() {
+		return tamanio;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public int getNumeroOjos() {
+		return numeroOjos;
+	}
+
+	public int getNumeroBrazos() {
+		return numeroBrazos;
+	}
+
+	public int getNumeroPies() {
+		return numeroPies;
+	}
+
+	public double getPrecioExtremidad() {
+		return precioExtremidad;
+	}
+
+	public double getPrecioOjo() {
+		return precioOjo;
+	}
+
+	public double getPrecioCuerpo() {
+		return precioCuerpo;
+	}
+
+	// Metodo Imprimir
+
+	public void imprimir() {
+		// ANSI códigos
+        String RESET = "\u001B[0m";
+        String BLACK_BG = "\u001B[40m";
+        String GREEN = "\u001B[32m";
+        String RED = "\u001B[31m";
+        String PURPLE = "\u001B[35m";
+        
+     
+        System.out.println(BLACK_BG + RED + "        °   °" + RESET);
+        System.out.println(BLACK_BG + RED + "        |   |" + RESET);
+        System.out.println(BLACK_BG + PURPLE + "      .-\"\"\"\"-." + RESET);
+        System.out.println(BLACK_BG + PURPLE + "     /        \\" + RESET);
+        System.out.println(BLACK_BG + GREEN  + "    |  .--. .--.|" + RESET);
+        System.out.println(BLACK_BG + RED    + "    | (o  )(  o)|" + RESET); // ojos rojos
+        System.out.println(BLACK_BG + GREEN  + "    |     ^^    |" + RESET);
+        System.out.println(BLACK_BG + RED + "       \\  --  /" + RESET);
+        System.out.println(BLACK_BG + GREEN  + "    |  '----'   |" + RESET);
+        System.out.println(BLACK_BG + PURPLE + "     \\________/" + RESET);
+        System.out.println(BLACK_BG + PURPLE + "      /  ||  \\" + RESET);
+        System.out.println(BLACK_BG + PURPLE + "     |   ||   |" + RESET);
+        System.out.println(BLACK_BG + PURPLE + "     |   ||   |" + RESET);
+        System.out.println(BLACK_BG + PURPLE + "     |   ||   |" + RESET);
+        System.out.println(BLACK_BG + PURPLE + "     '--------'" + RESET);
+
+        
+
+        
+		//
+		String datos="\nTamaño: "+tamanio+
+						"\nColor: "+color+
+						"\nNúmero de ojos: "+ numeroOjos+
+						"\nNúmero de brazos: "+ numeroBrazos+
+						"\nNúmero de pies: "+numeroPies+
+						"\nPrecio por extremidad: $"+ precioExtremidad+
+						"\nPrecio por ojo: $"+precioOjo+
+						"\nPrecio del cuerpo: $"+precioCuerpo;
+		System.out.println("*******************"+datos+"\n||||||||||||||||||");
+	}
+
+}
---
+++ src/com/krakedev/alien/test/TestConstructores.java
@@ -0,0 +1,13 @@
+package com.krakedev.alien.test;
+
+import com.krakedev.alien.Alien;
+
+public class TestConstructores {
+
+	public static void main(String[] args) {
+		Alien paul = new Alien(15,"verde");
+		paul.imprimir();
+
+	}
+
+}
